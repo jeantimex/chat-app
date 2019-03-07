57757498eb23e1d5ab1f1b749cb4e18cc0c7f878
jeantimex <jean.timex@gmail.com>
2019-03-06 22:54:22 -0800

    Initial project setup.

---
+++ chat-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chat-app
+//
+//  Created by Yong Su on 3/6/19.
+//  Copyright © 2019 Yong Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chat-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chat-app
+//
+//  Created by Yong Su on 3/6/19.
+//  Copyright © 2019 Yong Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
