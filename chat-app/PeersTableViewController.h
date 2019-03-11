8c5dc6af7cad68c1e6ca3758f03f372510a526c7
jeantimex <jean.timex@gmail.com>
2019-03-10 17:33:22 -0700

    Add peers ui tableview controller

---
+++ chat-app/PeersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeersTableViewController.h
+//  chat-app
+//
+//  Created by Yong Su on 3/10/19.
+//  Copyright © 2019 Yong Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeersTableViewController : UITableViewController
+
+@end
